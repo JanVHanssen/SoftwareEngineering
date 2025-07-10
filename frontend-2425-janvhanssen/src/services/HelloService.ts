5baf1c3290f51fb39da8f444a68a3905c0b1ddf9
JanVHanssen <r0906558@ucll.be>
2025-07-10 21:13:43 +0200

    connect vercel

--- frontend-2425-janvhanssen/src/services/HelloService.ts
+++ frontend-2425-janvhanssen/src/services/HelloService.ts
@@ -1,10 +1,14 @@
 export class HelloService {
   static async fetchHello(): Promise<string> {
-    const res = await fetch("http://localhost:8080/hello");
-    if (!res.ok) {
-      throw new Error("Fout bij ophalen van greeting");
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/hello`);
+      console.log("Fetching from:", `${process.env.NEXT_PUBLIC_API_URL}/hello`);
+      const data = await res.json();
+      console.log("Ontvangen data:", data);
+      return data.message;
+    } catch (error) {
+      console.error("Fout bij ophalen van hello:", error);
+      throw error;
     }
-    const data = await res.json();
-    return data.message;
   }
 }

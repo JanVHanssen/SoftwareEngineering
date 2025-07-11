00106ae638b068c2dead6130590b663e9cea78bf
JanVHanssen <r0906558@ucll.be>
2025-07-11 16:33:35 +0200

    FJLMKDJKJM

--- backend-2425-JanVHanssen/janvhanssen-backend/config/SecurityConfig.java
+++ backend-2425-JanVHanssen/janvhanssen-backend/config/SecurityConfig.java
@@ -28,10 +28,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://frontend-2425-janvhanssen.vercel.app")); // <-- Belangrijk!
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://frontend-2425-janvhanssen.vercel.app"));
+        config.setAllowCredentials(true);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);

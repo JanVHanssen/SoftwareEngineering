e9fb57e9a801d1198cb8dd487f3034e625e5ee3c
JanVHanssen <r0906558@ucll.be>
2025-07-11 08:19:07 +0200

    cors configuration

--- backend-2425-JanVHanssen/janvhanssen-backend/src/main/java/be/ucll/se/janvhanssenbackend/controller/GreetingController.java
+++ backend-2425-JanVHanssen/janvhanssen-backend/src/main/java/be/ucll/se/janvhanssenbackend/controller/GreetingController.java
@@ -3,16 +3,11 @@ package be.ucll.se.janvhanssenbackend.controller;
 import be.ucll.se.janvhanssenbackend.model.Greeting;
 import be.ucll.se.janvhanssenbackend.service.GreetingService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {
-        "http://localhost:3000",
-        "https://frontend-2425-janvhanssen.vercel.app"
-})
 @RequestMapping("/hello")
 public class GreetingController {
 

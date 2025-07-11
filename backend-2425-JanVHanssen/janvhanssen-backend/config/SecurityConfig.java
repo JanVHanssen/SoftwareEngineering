2861aafa87f9566a4f02cbc1c32a3205da59df05
JanVHanssen <r0906558@ucll.be>
2025-07-11 15:24:29 +0200

    update securityconfig

--- backend-2425-JanVHanssen/janvhanssen-backend/config/SecurityConfig.java
+++ backend-2425-JanVHanssen/janvhanssen-backend/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://frontend-2425-janvhanssen.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("https://frontend-2425-janvhanssen.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
@@ -37,4 +37,5 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
+
 }

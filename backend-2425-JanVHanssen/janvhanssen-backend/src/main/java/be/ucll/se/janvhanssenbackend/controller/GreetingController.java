0fb9373b27636536c872946482d21f53cfdf4d74
JanVHanssen <r0906558@ucll.be>
2025-07-14 17:45:54 +0200

    testtestetstets

--- backend-2425-JanVHanssen/janvhanssen-backend/src/main/java/be/ucll/se/janvhanssenbackend/controller/GreetingController.java
+++ backend-2425-JanVHanssen/janvhanssen-backend/src/main/java/be/ucll/se/janvhanssenbackend/controller/GreetingController.java
@@ -2,17 +2,14 @@ package be.ucll.se.janvhanssenbackend.controller;
 
 import be.ucll.se.janvhanssenbackend.model.Greeting;
 import be.ucll.se.janvhanssenbackend.service.GreetingService;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod; // Deze import toevoegen
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin(origins = "https://frontend-2425-janvhanssen.vercel.app", methods = { RequestMethod.GET,
+        RequestMethod.OPTIONS }, // Voeg OPTIONS toe voor preflight
+        allowedHeaders = "*", maxAge = 3600)
 public class GreetingController {
-
     private final GreetingService service;
 
     public GreetingController(GreetingService service) {

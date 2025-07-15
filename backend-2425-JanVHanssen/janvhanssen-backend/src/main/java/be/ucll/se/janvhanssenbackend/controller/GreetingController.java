397ac07b3cd37891beeb60c09b30f66d75785535
JanVHanssen <r0906558@ucll.be>
2025-07-15 20:10:53 +0200

    dsjhjkhfjkh

--- backend-2425-JanVHanssen/janvhanssen-backend/src/main/java/be/ucll/se/janvhanssenbackend/controller/GreetingController.java
+++ backend-2425-JanVHanssen/janvhanssen-backend/src/main/java/be/ucll/se/janvhanssenbackend/controller/GreetingController.java
@@ -2,6 +2,9 @@ package be.ucll.se.janvhanssenbackend.controller;
 
 import be.ucll.se.janvhanssenbackend.model.Greeting;
 import be.ucll.se.janvhanssenbackend.service.GreetingService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +21,14 @@ public class GreetingController {
     public Greeting getGreeting() {
         System.out.println("HELLO endpoint aangeroepen");
         return service.getGreetingById(1L);
+    }
+
+    @PostMapping("/api/cronjob")
+    public ResponseEntity<String> handleCronJob(@RequestHeader("Authorization") String authHeader) {
+        if (!authHeader.equals("Bearer " + System.getenv("CRON_SECRET"))) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+        }
+
+        return ResponseEntity.ok("Cron job executed successfully");
     }
 }

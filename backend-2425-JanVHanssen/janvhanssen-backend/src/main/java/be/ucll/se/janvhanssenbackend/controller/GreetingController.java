739060eeafb929e9dec283c3eaf71d0ff95cfe0e
JanVHanssen <r0906558@ucll.be>
2025-07-15 11:03:46 +0200

    qshjkdfhsjkqdh

--- backend-2425-JanVHanssen/janvhanssen-backend/src/main/java/be/ucll/se/janvhanssenbackend/controller/GreetingController.java
+++ backend-2425-JanVHanssen/janvhanssen-backend/src/main/java/be/ucll/se/janvhanssenbackend/controller/GreetingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
-@CrossOrigin(origins = "https://frontend-2425-janvhanssen.vercel.app", methods = { RequestMethod.GET,
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET,
         RequestMethod.OPTIONS }, // Voeg OPTIONS toe voor preflight
         allowedHeaders = "*", maxAge = 3600)
 public class GreetingController {
